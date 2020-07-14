308ec76fa09aae6a946b4f33e53dc54f5b7fab92
Kister Samuel <samuel.kister@gmail.com>
2020-07-14 10:37:08 +0200

    added Diagonal

---
+++ rgb/Diagonal.py
@@ -0,0 +1,32 @@
+import time
+from copy import deepcopy
+from rgb import LOCAL_RGB_MATRIX as rgb
+
+farbe = [200, 0, 0]
+
+led_array = rgb.RGBBasis()
+led_array.open_interface()
+
+matrix = led_array.rgb_matrix
+
+matrix[0][0] = farbe
+led_array.write2rgb()
+
+for _ in range(16):
+    time.sleep(0.3)
+
+    # Obere Zeile in untere kopieren
+    for zeile in range(7, 0, -1):
+        matrix[zeile] = deepcopy(matrix[zeile-1])
+
+    # Die erste Zeile um ein nach rechts schieben
+    for spalte in range(7, 0, -1):
+        matrix[0][spalte] = matrix[0][spalte-1]
+
+    # Und erste Zelle "auslöschen"
+    matrix[0][0] = [0, 0, 0]
+
+    led_array.write2rgb()
+
+led_array.close_interface()
+
